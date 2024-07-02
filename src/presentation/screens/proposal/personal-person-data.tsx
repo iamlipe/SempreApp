8850f0c80a6a2facb138eb51883ff54466f8a049
Felipe Lima <felipe_lima10@icloud.com>
2024-07-02 02:19:38 -0300

    feat: add proposal person data done

--- src/presentation/screens/proposal/personal-person-data.tsx
+++ src/presentation/screens/proposal/personal-person-data.tsx
@@ -24,6 +24,18 @@ export function ProposalPersonData() {
         setCanGo(true)
     }
 
+    console.log(data)
+
+    useEffect(() => {
+        setProposalNumber(data.proposalNumber)
+        setCustomerReference(data.customerReference)
+        setCompany(data.company)
+        setCnpj(data.cnpj)
+        setRequester(data.requester)
+        setEmail(data.email)
+        setPhone(data.phone)
+    }, [])
+
     useEffect(() => {
         if(canGo) {
             const fields: (keyof Proposal)[] = ['proposalNumber', 'customerReference', 'company', 'cnpj', 'requester', 'email', 'phone'];
@@ -48,17 +60,15 @@ export function ProposalPersonData() {
         }
     }, [data, canGo]);
 
-    console.log(errors)
-
     return (
         <div className="flex flex-col space-y-2">
-            <Input label="Número da Proposta" error={errors.proposalNumber} placeholder="Número da Proposta" type="text" onChange={(e) => setProposalNumber(e.target.value)} />
-            <Input label="Referência do Cliente" error={errors.customerReference}  placeholder="Referência do Cliente" type="text" onChange={(e) => setCustomerReference(e.target.value)} />
-            <Input label="Empresa" error={errors.company} placeholder="Empresa" type="text" onChange={(e) => setCompany(e.target.value)} />
-            <Input label="CNPJ" error={errors.cnpj} placeholder="CNPJ" type="text" onChange={(e) => setCnpj(e.target.value)} />
-            <Input label="Solicitante" error={errors.requester} placeholder="Solicitante" type="text" onChange={(e) => setRequester(e.target.value)} />
-            <Input label="Email" error={errors.email} placeholder="Email" type="text" onChange={(e) => setEmail(e.target.value)} />
-            <Input label="Telefone" error={errors.phone} placeholder="Telefone" type="text" onChange={(e) => setPhone(e.target.value)} />
+            <Input label="Número da Proposta" error={errors.proposalNumber} placeholder="Número da Proposta" type="text" onChange={(e) => setProposalNumber(e.target.value)} value={proposalNumber} />
+            <Input label="Referência do Cliente" error={errors.customerReference}  placeholder="Referência do Cliente" type="text" onChange={(e) => setCustomerReference(e.target.value)} value={customerReference} />
+            <Input label="Empresa" error={errors.company} placeholder="Empresa" type="text" onChange={(e) => setCompany(e.target.value)} value={company} />
+            <Input label="CNPJ" error={errors.cnpj} placeholder="CNPJ" type="text" onChange={(e) => setCnpj(e.target.value)} value={cnpj} />
+            <Input label="Solicitante" error={errors.requester} placeholder="Solicitante" type="text" onChange={(e) => setRequester(e.target.value)} value={requester} />
+            <Input label="Email" error={errors.email} placeholder="Email" type="text" onChange={(e) => setEmail(e.target.value)} value={email} />
+            <Input label="Telefone" error={errors.phone} placeholder="Telefone" type="text" onChange={(e) => setPhone(e.target.value)} value={phone} />
 
             <div className="flex flex-col">
                 <Button className="mt-10" variant="default" type="button" onClick={handlePersonalData}>Próximo</Button>
